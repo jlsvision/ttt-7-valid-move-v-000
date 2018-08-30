9b2717b8d0d6134802c6877bb13de3544608934d
Jorge Sanchez <sanchez0594.js@gmail.com>
2018-08-30 23:13:00 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,25 @@
-def valid_move?(board, position)
-  if position.to_i.between?(1,9)
-    if !position_taken?(board, position.to_i-1)
-      true
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
     end
   end
 
-  # position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
-end
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
 
-def position_taken?(board, location)
-  !(position(board, location).nil? || position(board, location) == " ")
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+
+
 end
